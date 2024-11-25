3ac3b4608ce7e597d2760da7ff6ada487cdc8bd0
sande11 <kelvinsande9@gmail.com>
2024-11-25 12:16:22 +0200

    "Updated JobDetails widget to use SingleChildScrollView, replaced Image.network with Image.asset, and added assets directory to pubspec.yaml."

--- lib/pages/job_details.dart
+++ lib/pages/job_details.dart
@@ -61,98 +61,107 @@ class JobDetails extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 300,
-                width: double.infinity,
-                child: Card(
-                  elevation: 4,
-                  color: const Color(0xFF2D82FF), // Card color
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                'https://via.placeholder.com/60', // Placeholder for logo
-                                height: 60,
-                                width: 60,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            const Expanded(
-                              child: Text(
-                                'Lilongwe Waterboard',
-                                style: TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 300,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 4,
+                    color: const Color(0xFF2D82FF), // Card color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.white, width: 1),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: Image.asset(
+                                    'lib/assets/logo.png',
+                                    height: 60,
+                                    width: 60,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        const Text('Position: ',
-                            style:
-                                TextStyle(fontSize: 18, color: Colors.white)),
-                        const SizedBox(height: 10),
-                        Text('Level: $level',
-                            style: const TextStyle(
-                                fontSize: 18, color: Colors.white)),
-                        const SizedBox(height: 10),
-                        Text('Location: $location',
-                            style: const TextStyle(
-                                fontSize: 18, color: Colors.white)),
-                        const SizedBox(height: 10),
-                        Text('Posted: $time',
-                            style: const TextStyle(
-                                fontSize: 18, color: Colors.white)),
-                        const SizedBox(height: 10),
-                        Text('Deadline: $type',
-                            style: const TextStyle(
-                                fontSize: 18, color: Colors.white)),
-                      ],
+                              const SizedBox(width: 10),
+                              const Expanded(
+                                child: Text(
+                                  'Lilongwe Waterboard',
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          const Text('Position: ',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white)),
+                          const SizedBox(height: 10),
+                          Text('Level: $level',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white)),
+                          const SizedBox(height: 10),
+                          Text('Location: $location',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white)),
+                          const SizedBox(height: 10),
+                          Text('Posted: $time',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white)),
+                          const SizedBox(height: 10),
+                          Text('Deadline: $type',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white)),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.only(left: 10.0),
-                child: Text(
-                  'About Job',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    'About Job',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(27, 27, 27, 1),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              const Padding(
-                padding: EdgeInsets.only(left: 10.0, right: 10),
-                child: Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam gravida quam quis consequat vulputate. Morbi ultrices in sem pretium sagittis. Suspendisse vestibulum orci et felis cursus, at dignissim justo fringilla. Vivamus ipsum sapien, semper quis vestibulum at, faucibus eu lacus. Vestibulum nulla eros, commodo eu scelerisque at, viverra nec nisl. In euismod ac enim sit amet scelerisque. Nullam bibendum vitae augue id facilisis. Nam fermentum ornare rutrum. Praesent quis nulla tortor. Curabitur ut ante risus. Maecenas eget odio id nisi sagittis lacinia at a ex. Mauris malesuada pharetra leo, quis finibus nisl. Praesent lobortis dapibus condimentum',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black,
+                const SizedBox(height: 5),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam gravida quam quis consequat vulputate. Morbi ultrices in sem pretium sagittis. Suspendisse vestibulum orci et felis cursus, at dignissim justo fringilla. Vivamus ipsum sapien, semper quis vestibulum at, faucibus eu lacus. Vestibulum nulla eros, commodo eu scelerisque at, viverra nec nisl. In euismod ac enim sit amet scelerisque. Nullam bibendum vitae augue id facilisis. Nam fermentum ornare rutrum. Praesent quis nulla tortor. Curabitur ut ante risus. Maecenas eget odio id nisi sagittis lacinia at a ex. Mauris malesuada pharetra leo, quis finibus nisl. Praesent lobortis dapibus condimentum',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromRGBO(27, 27, 27, 1),
+                    ),
+                    textAlign: TextAlign.justify,
                   ),
-                  textAlign: TextAlign.justify,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ));
   }
