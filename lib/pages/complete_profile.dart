9968cd63cd96d25feba7436163fbdab84229a578
sande11 <kelvinsande9@gmail.com>
2025-01-31 23:46:16 +0200

    complete profile UI changes

--- lib/pages/complete_profile.dart
+++ lib/pages/complete_profile.dart
@@ -6,13 +6,13 @@ class CompleteProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        backgroundColor: Colors.grey[200],
-        elevation: 2,
+        backgroundColor: Colors.blueAccent,
+        elevation: 3,
         title: const Text(
           "Complete Profile",
-          style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(27, 27, 27, 1)),
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
         centerTitle: true,
       ),
@@ -30,148 +30,304 @@ class CompleteProfileForm extends StatefulWidget {
 
 class _CompleteProfileFormState extends State<CompleteProfileForm> {
   final _formKey = GlobalKey<FormState>();
+
+  // Personal Details Controllers
   final TextEditingController _fullNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _locationController = TextEditingController();
-  final TextEditingController _professionController = TextEditingController();
-  final TextEditingController _experienceController = TextEditingController();
+
+  // Work & Education
+  List<Map<String, TextEditingController>> _workHistory = [];
+  List<TextEditingController> _professions = [];
+  List<TextEditingController> _educations = [];
+  List<TextEditingController> _skills = [];
+  List<TextEditingController> _jobPreferences = [];
+
+  // Links & Resume Controllers
   final TextEditingController _resumePathController = TextEditingController();
-  final TextEditingController _skillsController = TextEditingController();
   final TextEditingController _linkedinUrlController = TextEditingController();
   final TextEditingController _portfolioUrlController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
-  final TextEditingController _jobPreferencesController =
-      TextEditingController();
-  final TextEditingController _educationController = TextEditingController();
-  final TextEditingController _workHistoryController = TextEditingController();
-
-  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.only(
-        top: 25,
-        left: 16,
-        right: 16,
-        bottom: MediaQuery.of(context).viewInsets.bottom + 16,
-      ),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            // Profile Picture Upload
-            Center(
-              child: Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.grey[400],
-                    backgroundImage:
-                        const AssetImage('assets/profile_placeholder.png'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              _buildProfilePicture(),
+              const SizedBox(height: 15),
+              _buildSection(title: 'Personal Information', children: [
+                _buildTextField(_fullNameController, 'Full Name', Icons.person),
+                const SizedBox(
+                  height: 10,
+                ),
+                _buildTextField(_emailController, 'Email', Icons.email,
+                    keyboardType: TextInputType.emailAddress),
+                const SizedBox(
+                  height: 10,
+                ),
+                _buildTextField(_phoneController, 'Phone', Icons.phone,
+                    keyboardType: TextInputType.phone),
+                const SizedBox(
+                  height: 10,
+                ),
+                _buildTextField(
+                    _locationController, 'Address', Icons.location_on),
+              ]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: _buildSection(title: 'Work & Education', children: [
+                  _buildWorkHistory(),
+                  _buildDynamicField("Profession", _professions),
+                  _buildDynamicField("Education", _educations),
+                  _buildDynamicField("Skills", _skills),
+                  _buildDynamicField("Job Preferences", _jobPreferences),
+                ]),
+              ),
+              _buildSection(title: 'Links & Resume', children: [
+                _buildTextField(_resumePathController, 'Resume Path (Upload)',
+                    Icons.upload_file),
+                const SizedBox(
+                  height: 10,
+                ),
+                _buildTextField(
+                    _linkedinUrlController, 'LinkedIn URL', Icons.link),
+                const SizedBox(
+                  height: 10,
+                ),
+                _buildTextField(
+                    _portfolioUrlController, 'Portfolio URL', Icons.link),
+              ]),
+              _buildSection(title: 'Bio', children: [
+                TextFormField(
+                  controller: _bioController,
+                  decoration: InputDecoration(
+                    labelText: 'Short Bio',
+                    prefixIcon: const Icon(Icons.description),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
                   ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: GestureDetector(
-                      onTap: () {
-                        // Add image upload logic here
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: const BoxDecoration(
-                          color: Colors.blue,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(Icons.camera_alt,
-                            color: Colors.white, size: 20),
-                      ),
+                  maxLines: 3,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter Short Bio';
+                    }
+                    return null;
+                  },
+                ),
+              ]),
+              const SizedBox(height: 25),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  elevation: 3,
+                ),
+                onPressed: _submitForm,
+                child: const Text(
+                  'Submit Profile',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSection(
+      {required String title, required List<Widget> children}) {
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 600, // Adjust width for larger screens
+      ),
+      child: Card(
+        margin: const EdgeInsets.only(bottom: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent)),
+              const SizedBox(height: 10),
+              ...children,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfilePicture() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        const CircleAvatar(
+            radius: 55,
+            backgroundColor: Colors.grey,
+            child: Icon(Icons.person, size: 60, color: Colors.white)),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: CircleAvatar(
+            backgroundColor: Colors.blueAccent,
+            radius: 18,
+            child: IconButton(
+              icon: const Icon(Icons.camera_alt, size: 16, color: Colors.white),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWorkHistory() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Work History",
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+        const SizedBox(height: 8),
+        ..._workHistory.asMap().entries.map((entry) {
+          int index = entry.key;
+          var data = entry.value;
+          return Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                children: [
+                  _buildTextField(
+                      data['Company']!, 'Company Name', Icons.business),
+                  _buildTextField(data['Position']!, 'Position', Icons.work),
+                  _buildTextField(
+                      data['Years']!, 'Years Worked', Icons.calendar_today,
+                      keyboardType: TextInputType.number),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () =>
+                          setState(() => _workHistory.removeAt(index)),
                     ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 20),
-
-            _buildTextField(_fullNameController, 'Full Name'),
-            _buildTextField(_emailController, 'Email',
-                keyboardType: TextInputType.emailAddress),
-            _buildTextField(_phoneController, 'Phone',
-                keyboardType: TextInputType.phone),
-            _buildTextField(_locationController, 'Location'),
-            _buildTextField(_professionController, 'Profession'),
-            _buildTextField(_experienceController, 'Experience (Years)',
-                keyboardType: TextInputType.number),
-            _buildTextField(_resumePathController, 'Resume Path'),
-            _buildTextField(_skillsController, 'Skills'),
-            _buildTextField(_linkedinUrlController, 'LinkedIn URL'),
-            _buildTextField(_portfolioUrlController, 'Portfolio URL'),
-            _buildTextField(_bioController, 'Bio'),
-            _buildTextField(_jobPreferencesController, 'Job Preferences'),
-            _buildTextField(_educationController, 'Education'),
-            _buildTextField(_workHistoryController, 'Work History'),
-
-            const SizedBox(height: 20),
-
-            // Submit Button
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                onPressed: _isLoading ? null : _submitForm,
-                child: _isLoading
-                    ? const CircularProgressIndicator(color: Colors.white)
-                    : const Text(
-                        'Submit',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-              ),
-            ),
-          ],
+          );
+        }).toList(),
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blueAccent,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+          icon: const Icon(Icons.add, color: Colors.white),
+          label: const Text("Add Work History",
+              style: TextStyle(color: Colors.white)),
+          onPressed: () {
+            setState(() {
+              _workHistory.add({
+                'Company': TextEditingController(),
+                'Position': TextEditingController(),
+                'Years': TextEditingController(),
+              });
+            });
+          },
         ),
-      ),
+      ],
     );
   }
 
-  // Common text field builder
-  Widget _buildTextField(TextEditingController controller, String labelText,
-      {TextInputType keyboardType = TextInputType.text}) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
-      child: TextFormField(
-        controller: controller,
-        decoration: InputDecoration(
-          labelText: labelText,
-          border: OutlineInputBorder(),
-          filled: true,
-          fillColor: Colors.white,
+  Widget _buildDynamicField(
+      String label, List<TextEditingController> controllers) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label,
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+        const SizedBox(height: 8),
+        ...controllers.asMap().entries.map((entry) {
+          int index = entry.key;
+          return Row(
+            children: [
+              Expanded(child: _buildTextField(entry.value, label, Icons.edit)),
+              IconButton(
+                icon: const Icon(Icons.delete, color: Colors.red),
+                onPressed: () => setState(() => controllers.removeAt(index)),
+              ),
+            ],
+          );
+        }).toList(),
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blueAccent,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+          icon: const Icon(Icons.add, color: Colors.white),
+          label:
+              Text("Add $label", style: const TextStyle(color: Colors.white)),
+          onPressed: () =>
+              setState(() => controllers.add(TextEditingController())),
         ),
-        keyboardType: keyboardType,
-        validator: (value) =>
-            value == null || value.isEmpty ? 'Please enter $labelText' : null,
-      ),
+      ],
+    );
+  }
+
+  Widget _buildTextField(
+      TextEditingController controller, String label, IconData icon,
+      {TextInputType keyboardType = TextInputType.text,
+      InputDecoration? decoration}) {
+    return TextFormField(
+      controller: controller,
+      decoration: decoration ??
+          InputDecoration(
+            labelText: label,
+            prefixIcon: Icon(icon),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+      keyboardType: keyboardType,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter $label';
+        }
+        return null;
+      },
     );
   }
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
-      setState(() => _isLoading = true);
-
-      // TODO: Handle form submission
-
-      Future.delayed(const Duration(seconds: 2), () {
-        setState(() => _isLoading = false);
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Profile updated successfully!")),
-        );
-      });
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Profile Updated Successfully! 🎉")));
     }
   }
 }
